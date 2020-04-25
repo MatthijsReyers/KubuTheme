bef15cd522f4e4ce8bf726c1a6b65b4eed81899f
matthijsreyers <matthijsreyers@gmail.com>
2020-04-25 21:10:14 +0200

    fixed Terminal icon link name

--- IconTheme/kubu/actions/linkmaker.py
+++ IconTheme/kubu/actions/linkmaker.py
@@ -6,6 +6,12 @@ icons = dict()
 icons = {
     "system-reboot.svg":[
         "system-restart.svg"
+    ],
+    "system-switch-user.svg":[
+        "xfsm-switch.svg",
+        "org.kde.plasma.userswitch.svg",
+        "org.kde.plasma.userswitcher.svg",
+        "org.kde.plasma.uswitcher.svg"
     ]
 }
 
---
+++ Kde/konsole/Kubu.profile
@@ -0,0 +1,7 @@
+[Appearance]
+ColorScheme=Kubu
+Font=Noto Mono,10,-1,0,50,0,0,0,0,0
+
+[General]
+Name=Kubu
+Parent=FALLBACK/
